9b1b499edc46a8317c2bf1bb7e9b827df5f106db
universuen <52519513+universuen@users.noreply.github.com>
2022-12-26 18:20:35 +0800

    Create original_erm.py

---
+++ scripts/original_erm.py
@@ -0,0 +1,22 @@
+import context
+
+from GOOD.kernel.main import *
+
+if __name__ == '__main__':
+    args = args_parser(
+        [
+            '--config_path', 'configs/GOOD_configs/GOODMotif/basis/covariate/ERM.yaml'
+        ]
+    )
+    config = config_summoner(args)
+    load_logger(config)
+
+    model, loader = initialize_model_dataset(config)
+    ood_algorithm = load_ood_alg(config.ood.ood_alg, config)
+
+    pipeline = load_pipeline(config.pipeline, config.task, model, loader, ood_algorithm, config)
+    pipeline.load_task()
+
+    if config.task == 'train':
+        pipeline.task = 'test'
+        pipeline.load_task()
